85a5a144fa1371cbb70881e11c37a38d0264ddba
pietro <pietro.bozzo1998@gmail.com>
2026-04-27 10:44:55 +0200

    bugfix

--- Plot/plotDistr.m
+++ Plot/plotDistr.m
@@ -1,20 +1,20 @@
 function plotDistr(data,opt)
-% plotDistr Plot probability density function of data
+% plotDistr Plot probability density function (pdf) of data
 %
 % repeating arguments
-%
+%     data        (:,1), data to draw distribution of, a curve is drawn for each 'data' provided
 %
 % name-value arguments
-%     nbins
-%     polar       logical = false, make [0,2*pi] edges
+%     nbins       int = 0, number of bins to compute pdf, default is handled by <a href="matlab:help histcounts">histcounts</a>
+%     polar       logical = false, make [0,2*pi] periodic edges for data in radians
 %     log         logical = false, plot on loglog axis
-%     label       (:,1) string = [], legend labels
-%     color       (:,3), RGB color matrix, one for each element of data
-%     lineprop    (:,1) cell = {}, properties passed to plot()
-%     name        string = "" % name of the quantity plotted
-%     unit        string = ""
-%     type        string = "pdf"
-%     ax
+%     label       (:,1) string = [], legend labels, one for each 'data'
+%     color       (:,3), RGB color matrix, one for each 'data'
+%     lineprop    (:,1) cell = {}, properties passed to <a href="matlab:help plot">plot</a>
+%     name        string = "", name of the plotted quantity to make x axis label
+%     unit        string = "", units of measurement of the plotted quantity to make x axis label
+%     type        string = "pdf", passed to option 'Normalization' of <a href="matlab:help histcounts">histcounts</a>
+%     ax          axis = gca, axes to plot in
 
 arguments (Repeating)
   data (:,1) {mustBeNumeric}
@@ -22,14 +22,14 @@ end
 arguments
   opt.nbins (1,1) {mustBeNumeric,mustBeInteger,mustBeNonnegative} = 0
   opt.polar (1,1) {mustBeLogical} = false
-  opt.log (1,1) {mustBeLogical} = false % plot on loglog axis
-  opt.label (:,1) string = [] % legend labels
+  opt.log (1,1) {mustBeLogical} = false
+  opt.label (:,1) string = []
   opt.color (:,3) {mustBeNumeric,mustBeNonnegative,mustBeLessThanOrEqual(opt.color,1)} = myColors(1:numel(data))
   opt.lineprop (:,1) cell = {}
-  opt.name (1,1) string = "" % name of the quantity plotted
+  opt.name (1,1) string = ""
   opt.unit (1,1) string = ""
   opt.type (1,1) string = "pdf"
-  opt.ax (1,1) matlab.graphics.axis.Axes = gca % figure axes
+  opt.ax (1,1) matlab.graphics.axis.Axes = gca
 end
 
 if ~isempty(opt.label) && numel(data) ~= numel(opt.label)
@@ -49,16 +49,16 @@ for i = numel(data) : -1 : 1
     % remap angles in [0,2π)
     angles = mod(data{i},2*pi);
     edges = linspace(0,2*pi,opt.nbins);
-    [counts,edges] = histcounts(angles,edges,Normalization=opt.type);
+    [counts,edges] = histcounts(angles,edges,'Normalization',opt.type);
     % duplicate cycle
     counts = [counts,counts];
     edges = [edges,edges(end)+edges(2:end)];
     % adjust x axes
     set(opt.ax,'XLim',[0,4*pi],'XTick',[0,pi,2*pi,3*pi,4*pi],'XTickLabel',["0","π","2π","3π","4π"])
   elseif opt.nbins ~= 0
-    [counts,edges] = histcounts(data{i},opt.nbins,Normalization=opt.type);
+    [counts,edges] = histcounts(data{i},opt.nbins,'Normalization',opt.type);
   else
-    [counts,edges] = histcounts(data{i},Normalization=opt.type);
+    [counts,edges] = histcounts(data{i},'Normalization',opt.type);
   end
   % center x points in bins
   x = (edges(1:end-1) + edges(2:end)) / 2;
